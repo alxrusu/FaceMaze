23cc0447d029fd8a5191108e3977d79d1161d6b5
alxrusu <alexrusu96@gmail.com>
2016-06-03 18:16:28 +0300

    Server Integration
    - server now decides the path the ball is following
    - added support for click events (scarcely) (just up - down, for above
    and below the primary diagonal)

--- nodejs/node_server.js
+++ nodejs/node_server.js
@@ -2,9 +2,30 @@ var ws = require("nodejs-websocket")
 console.log(ws)
 var server = ws.createServer(function (conn) {
     console.log("New connection")
+    var dx=0;
+    var dy=0;
     conn.on("text", function (str) {
         console.log("Received "+str)
-        conn.sendText(str.toUpperCase()+"!!!")
+
+        if (str.localeCompare("left")==0){
+        	dx=-2;
+        	dy=0;
+        }
+        if (str.localeCompare("right")==0){
+        	dx=2;
+        	dy=0;
+        }
+        if (str.localeCompare("up")==0){
+        	dy=-2;
+        	dx=0;
+        }
+        if (str.localeCompare("down")==0){
+        	dx=0;
+        	dy=2;
+        }
+
+
+        conn.sendText(dx.toString()+";"+dy.toString())
     })
     conn.on("close", function (code, reason) {
         console.log("Connection closed")
